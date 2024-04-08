3d893585e6c0561116f1a07707c64a5cd6ad10ca
Timmy1404 <148655506+Timmy1404@users.noreply.github.com>
2024-04-08 19:21:55 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// Find the laterst version by visiting https://unpkg.com/three.
+
+    import * as THREE from 'https://unpkg.com/three@0.163.0/build/three.module.js';
+
+    const scene = new THREE.Scene();
+    console.log(scene);

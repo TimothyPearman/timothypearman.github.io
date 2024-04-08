bc07b140784503bf0ad9e88ddd81180f0b9656bb
Timmy1404 <148655506+Timmy1404@users.noreply.github.com>
2024-04-08 19:27:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 // Find the laterst version by visiting https://unpkg.com/three.
+import * as THREE from 'https://unpkg.com/three@0.163.0/build/three.module.js';
 
-    import * as THREE from 'https://unpkg.com/three@0.163.0/build/three.module.js';
-
-    const scene = new THREE.Scene();
-    console.log(scene);
+const scene = new THREE.Scene();
+const camera = new THREE.
+    PerspectiveCamera(
+    75,
+    innerWidth / innerHeight,
+    0.1,
+    1000
+)
+const renderer = new THREE.WebGLRenderer(  
+)
+console.log(scene);
+console.log(camera);
+console.log(renderer);

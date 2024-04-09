640ac574a8108d284964d9218261474aa2059cd5
Timmy1404 <148655506+Timmy1404@users.noreply.github.com>
2024-04-09 14:26:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 import * as THREE from 'https://unpkg.com/three@0.163.0/build/three.module.js';
 //import { OrbitControls } from 'https://unpkg.com/three/examples/jsm/controls/OrbitControls.js';
+//console.log(scene);  //checking if the orbit controls import worked
 
 const scene = new THREE.Scene()  //declaring the thingys
 const camera = new THREE.PerspectiveCamera(

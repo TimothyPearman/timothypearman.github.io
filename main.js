390a1dc8dd84aed431daea1ee04886e9dd4d86e2
Timmy1404 <148655506+Timmy1404@users.noreply.github.com>
2024-04-09 10:37:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 // Find the latest version by visiting https://unpkg.com/three.
-
 import * as THREE from 'https://unpkg.com/three@0.163.0/build/three.module.js'; //three.js library
-import { OrbitControls } from 'https://unpkg.com/three@0.163.0/examples/jsm/controls/OrbitControls.js'  //orbit controls module
-
-console.log(OrbitControls);
+//import { OrbitControls } from 'https://unpkg.com/three@0.163.0/examples/jsm/controls/OrbitControls.js'  //orbit controls module
+//console.log(OrbitControls);
 
 const scene = new THREE.Scene()  //declaring the thingys
 const camera = new THREE.PerspectiveCamera(
@@ -30,8 +28,7 @@ const mesh = new THREE.Mesh(boxGeometry, material)  //create the box mesh using 
 //console.log(mesh);
 
 scene.add(mesh)  //check box mesh
-
-new OrbitControls(camera, renderer.domElement)
+//new OrbitControls(camera, renderer.domElement)
 camera.position.z = 5  //move camera away from origin to see the box
 
 function animate() {  //animation loop

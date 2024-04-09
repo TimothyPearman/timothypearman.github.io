d8adbfd3d721313b413d7bf11fc247c6e68c1e32
Timmy1404 <148655506+Timmy1404@users.noreply.github.com>
2024-04-09 09:54:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 import * as THREE from 'https://unpkg.com/three@0.163.0/build/three.module.js';
 
-const scene = new THREE.Scene()
+const scene = new THREE.Scene()  //declaring the thingys
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -10,31 +10,32 @@ const camera = new THREE.PerspectiveCamera(
   1000
 )
 const renderer = new THREE.WebGLRenderer()
-console.log(scene);
+console.log(scene);  //checking the thingys work
 console.log(camera);
 console.log(renderer);
 
-renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setSize(window.innerWidth, window.innerHeight)  //make it take up the full screen
+renderer.setPixelRatio(devicePixelRatio)  //fix sharp edges
 document.body.appendChild(renderer.domElement)
 
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({color: 0x00FF00})
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1) //create the box geometry
+const material = new THREE.MeshBasicMaterial({color: 0x00FF00})  //create the box material
 console.log(boxGeometry);
 console.log(material);
 
-const mesh = new THREE.Mesh(boxGeometry, material)
+const mesh = new THREE.Mesh(boxGeometry, material)  //create the box mesh using the geometry and material
 console.log(mesh);
 
-scene.add(mesh)
+scene.add(mesh)  //check box mesh
 
-camera.position.z = 5
+camera.position.z = 5  //move camera away from origin to see the box
 
-function animate() {
+function animate() {  //animation loop
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
-  mesh.rotation.x += 1 
-  mesh.rotation.y += 1 
-  mesh.rotation.z += 1 
+  mesh.rotation.x += 0.01 
+  mesh.rotation.y += 0.01 
+  mesh.rotation.z += 0.01 
 }
 
 animate()

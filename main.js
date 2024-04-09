3b1c32782088c459b7ba32eb6cbe2303db86e2e6
Timmy1404 <148655506+Timmy1404@users.noreply.github.com>
2024-04-09 11:30:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // Find the latest version by visiting https://unpkg.com/three.
 
 import * as THREE from 'https://unpkg.com/three@0.163.0/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.163.0/examples/jsm/controls/OrbitControls.js'  //orbit controls module
-console.log(OrbitControls);
+//import { OrbitControls } from 'https://unpkg.com/three@0.163.0/examples/jsm/controls/OrbitControls.js'  //orbit controls module
+//console.log(OrbitControls);
 
 const scene = new THREE.Scene()  //declaring the thingys
 const camera = new THREE.PerspectiveCamera(
@@ -39,7 +39,7 @@ function animate() {  //animation loop
   mesh.rotation.y += 0.01 
   mesh.rotation.z += 0.01 
 
-  orbitControls.update();  // Update controls
+  //orbitControls.update();  // Update controls
 }
 
 animate()

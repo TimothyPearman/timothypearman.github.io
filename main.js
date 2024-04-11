aae3c17fef397ac2f26615f541a4da5d094a35d4
Timmy1404 <148655506+Timmy1404@users.noreply.github.com>
2024-04-11 23:06:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,130 +1,140 @@
 //Three.js libary
-            import * as THREE from 'https://unpkg.com/three@0.163.0/build/three.module.js';
-            //Orbit Contol module
-            import {OrbitControls} from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
-            
-            
-            var scene, camera, renderer, mesh;
-var meshFloor, ambientLight, light;
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+//Orbit Contol module
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
+//import .gltf files
+import { GLTFLoader } from 'https://unpkg.com/three@0.126.1/examples/jsm/loaders/GLTFLoader.js';
 
-var crate, crateTexture, crateNormalMap, crateBumpMap;
+//creating the structuere
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000)
+const renderer = new THREE.WebGLRenderer({antialias:true})
+const controls = new OrbitControls(camera, renderer.domElement);
+//
+controls.target = new THREE.Vector3(0, 0, 0);
+controls.enableRotate = true;
+controls.enablePan = true;
+controls.enableDamping = true;
+controls.dampingFactor = 0.5;
+controls.rotateSpeed = 0.5;
+controls.panSpeed = 1.1;
+controls.minDistance = 1;
+controls.maxDistance = 20;
+//controls.minAzimuthAngle = 0;
+//controls.maxAzimuthAngle = 0;
+//controls.minPolarAngle = 0;
+//controls.maxPolarAngle = 0;
+//controls.autoRotate = true;
+//controls.autoRotateSpeed = 5;  
 
-var keyboard = {};
-var player = { height:1.8, speed:0.2, turnSpeed:Math.PI*0.02 };
-var USE_WIREFRAME = false;
+renderer.setSize(window.innerWidth, window.innerHeight)  //make it take up the full screen
+renderer.setPixelRatio(devicePixelRatio)  //fix sharp edges
+document.body.appendChild(renderer.domElement)  //append to HTML <body>
 
-function init(){
-	scene = new THREE.Scene();
-	camera = new THREE.PerspectiveCamera(90, 1280/720, 0.1, 1000);
-	
-	mesh = new THREE.Mesh(
-		new THREE.BoxGeometry(1,1,1),
-		new THREE.MeshPhongMaterial({color:0xff4444, wireframe:USE_WIREFRAME})
-	);
-	mesh.position.y += 1;
-	mesh.receiveShadow = true;
-	mesh.castShadow = true;
-	scene.add(mesh);
-	
-	meshFloor = new THREE.Mesh(
-		new THREE.PlaneGeometry(20,20, 10,10),
-		new THREE.MeshPhongMaterial({color:0xffffff, wireframe:USE_WIREFRAME})
-	);
-	meshFloor.rotation.x -= Math.PI / 2;
-	meshFloor.receiveShadow = true;
-	scene.add(meshFloor);
-	
-	ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
-	scene.add(ambientLight);
-	
-	light = new THREE.PointLight(0xffffff, 0.8, 18);
-	light.position.set(-3,6,-3);
-	light.castShadow = true;
-	light.shadow.camera.near = 0.1;
-	light.shadow.camera.far = 25;
-	scene.add(light);
-	
-	
-	// Texture Loading
-	var textureLoader = new THREE.TextureLoader();
-	crateTexture = textureLoader.load("crate0/crate0_diffuse.png");
-	crateBumpMap = textureLoader.load("crate0/crate0_bump.png");
-	crateNormalMap = textureLoader.load("crate0/crate0_normal.png");
-	
-	// Create mesh with these textures
-	crate = new THREE.Mesh(
-		new THREE.BoxGeometry(3,3,3),
-		new THREE.MeshPhongMaterial({
-			color:0xffffff,
-			
-			map:crateTexture,
-			bumpMap:crateBumpMap,
-			normalMap:crateNormalMap
-		})
-	);
-	scene.add(crate);
-	crate.position.set(2.5, 3/2, 2.5);
-	crate.receiveShadow = true;
-	crate.castShadow = true;
-	
-	
-	camera.position.set(0, player.height, -5);
-	camera.lookAt(new THREE.Vector3(0,player.height,0));
-	
-	renderer = new THREE.WebGLRenderer();
-	renderer.setSize(1280, 720);
+//skybox
+const skyboxgeometry = new THREE.BoxGeometry(10000, 10000, 10000);
+const skyboxmaterials = [
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('skybox/1.png'), side: THREE.DoubleSide}),  //right of camera
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('skybox/left.png'), side: THREE.DoubleSide}),  //left of camera
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('skybox/3.png'), side: THREE.DoubleSide}),  //top of camera
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('skybox/4.png'), side: THREE.DoubleSide}),  //bottom of camera
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('skybox/5.png'), side: THREE.DoubleSide}),  //back of camera
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('skybox/6.png'), side: THREE.DoubleSide})  //front of camera
+];
+const skyboxmaterial = new THREE.MeshFaceMaterial(skyboxmaterials);
+const skybox = new THREE.Mesh(skyboxgeometry, skyboxmaterial);
+//
+skybox.position.set(0, 0, 0);
+skybox.rotation.set(0, 0, 0);
 
-	renderer.shadowMap.enabled = true;
-	renderer.shadowMap.type = THREE.BasicShadowMap;
-	
-	document.body.appendChild(renderer.domElement);
-	
-	animate();
+//plane
+const planegeometry = new THREE.PlaneGeometry(5, 5);
+const planematerial = new THREE.MeshBasicMaterial( {color: 0x00FFFF, side: THREE.DoubleSide} );
+const plane = new THREE.Mesh(planegeometry, planematerial);
+//
+plane.position.set(0, 0, 0);
+plane.rotation.set(270 * (Math.PI / 180), 0, 0);
+
+//box
+const boxgeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxmaterial = new THREE.WireframeGeometry(boxgeometry);
+const box = new THREE.LineSegments(boxmaterial);
+box.material.depthTest = false;
+box.material.opacity = 1;
+box.material.transparent = true;
+//
+box.position.set(-2, 0.5, -2);
+box.rotation.set(0, 0, 0);
+
+//sphere
+const sphereGeometry = new THREE.SphereGeometry(1, 4, 4)
+const spherematerial = new THREE.MeshPhongMaterial({color: 0xFFFF00})
+const sphere = new THREE.Mesh(sphereGeometry, spherematerial)
+//
+sphere.position.set(0, 2, 0);
+sphere.rotation.set(0, 0, 0);
+
+const gltfLoader = new GLTFLoader();
+gltfLoader.setPath('./model/2018_flat_screen_tv/');
+gltfLoader.load('scene.gltf', (gltfScene) => {
+    //
+    gltfScene.scene.position.set(1.5, 0.75, -2)
+    gltfScene.scene.rotation.set(0, 0, 0)
+    gltfScene.scene.scale.set(1, 1, 1)
+    //
+    scene.add(gltfScene.scene);
+});
+
+//top light1
+const light1 = new THREE.DirectionalLight(0xFFFFFF, 0.75)
+//
+light1.position.set(0, 5, 0)
+light1.rotation.set(0, 0, 0)
+//top light2
+const light2 = new THREE.DirectionalLight(0xFFFFFF, 0.75)
+//
+light2.position.set(5, 5, 5)
+light2.rotation.set(0, 0, 0)
+
+//user camera
+camera.position.set(0, 5, 10)
+camera.rotation.set(0, 0, 0)
+//
+
+camera.lookAt(0, 0, 0);
+controls.update();
+
+//creating the scene
+scene.add(skybox)
+scene.add(plane)  
+scene.add(box);
+scene.add(sphere)
+scene.add(light1)
+scene.add(light2)
+
+controls.saveState();
+window.addEventListener('keydown', function(e) {
+    if(e.code === 'KeyS') //save view
+        controls.saveState();
+    if(e.code === 'KeyL') //go back to saved view
+        for (let i = 0; i < 10; i++) {
+            controls.reset();
+        }
+        
+});
+
+function animate() {  //animation loop
+    renderer.render(scene, camera)
+
+    sphere.rotation.x += 0.0001 
+    sphere.rotation.y += 0.0001 
+    sphere.rotation.z += 0.0001 
+
+    skybox.rotation.x += 0.0001 
+    skybox.rotation.y += 0.0001 
+    skybox.rotation.z += 0.0001 
+  
+    requestAnimationFrame(animate)
 }
 
-function animate(){
-	requestAnimationFrame(animate);
-	
-	mesh.rotation.x += 0.01;
-	mesh.rotation.y += 0.02;
-	crate.rotation.y += 0.01;
-	
-	if(keyboard[87]){ // W key
-		camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
-		camera.position.z -= -Math.cos(camera.rotation.y) * player.speed;
-	}
-	if(keyboard[83]){ // S key
-		camera.position.x += Math.sin(camera.rotation.y) * player.speed;
-		camera.position.z += -Math.cos(camera.rotation.y) * player.speed;
-	}
-	if(keyboard[65]){ // A key
-		camera.position.x += Math.sin(camera.rotation.y + Math.PI/2) * player.speed;
-		camera.position.z += -Math.cos(camera.rotation.y + Math.PI/2) * player.speed;
-	}
-	if(keyboard[68]){ // D key
-		camera.position.x += Math.sin(camera.rotation.y - Math.PI/2) * player.speed;
-		camera.position.z += -Math.cos(camera.rotation.y - Math.PI/2) * player.speed;
-	}
-	
-	if(keyboard[37]){ // left arrow key
-		camera.rotation.y -= player.turnSpeed;
-	}
-	if(keyboard[39]){ // right arrow key
-		camera.rotation.y += player.turnSpeed;
-	}
-	
-	renderer.render(scene, camera);
-}
-
-function keyDown(event){
-	keyboard[event.keyCode] = true;
-}
-
-function keyUp(event){
-	keyboard[event.keyCode] = false;
-}
-
-window.addEventListener('keydown', keyDown);
-window.addEventListener('keyup', keyUp);
-
-window.onload = init;
+animate()
